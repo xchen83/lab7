f3fa3c2e2551c73bc3105b0025ba81d7be4afb96
Xuan Chen <148398101+xchen83@users.noreply.github.com>
2023-11-28 14:28:45 -0800

    Update cli.py

--- cli.py
+++ cli.py
@@ -1,12 +1,15 @@
-# This file contains the Command Line Interface (CLI) for
-# the Tic-Tac-Toe game. This is where input and output happens.
-# For core game logic, see logic.py.
 from logic import Game, Human, Bot
 
 if __name__ == '__main__':
+    num_players = Game()
+
     game = Game()
     human_player = Human()
-    bot_player = Bot()
+
+    if num_players == 1:
+        bot_player = Bot()
+    else:
+        bot_player = Human()
 
     print("Welcome to Tic-Tac-Toe!")
 
@@ -16,14 +19,16 @@ if __name__ == '__main__':
             number_picked = human_player.make_move()
             if 1 <= number_picked <= 9:
                 game.modify_array(number_picked)
-                game.other_player('X')
+                game.other_player()
+                game.print_board()
             else:
                 print("Invalid input. Please try again.")
             game.turnCounter += 1
         else:
-            other_player_choice = bot_player.make_move()
+            other_player_choice = bot_player.make_move() if num_players == 1 else human_player.make_move()
             print("\nOther Player's Choice:", other_player_choice)
             game.modify_array(other_player_choice)
+            game.other_player()
             game.turnCounter += 1
 
         winner = game.get_winner()
